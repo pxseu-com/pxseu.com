cb90ea06c00034d3cfac96461ced079ffa16037f
pxseu <57842793+pxseu@users.noreply.github.com>
2024-07-16 19:43:51 +0200

    fix ts issue

--- packages/api/src/db/redis/index.ts
+++ packages/api/src/db/redis/index.ts
@@ -1,9 +1,8 @@
 import Redis from "ioredis";
 import { API_HOSTNAME, REDIS_URL } from "../../config";
 
-export const redis = new Redis({
+export const redis = new Redis(REDIS_URL,{
 	keyPrefix: `${API_HOSTNAME}:`,
-	url: REDIS_URL,
 });
 
 redis.on("error", (error) => {
